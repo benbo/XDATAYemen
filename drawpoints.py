d8a3f755b54648cb7f4354958096448ca3f2b371
Dougal J. Sutherland <dougal@gmail.com>
2016-05-25 15:56:57 -0400

    fix up drawpoints's page, including a ridiculous version of drawing lines

--- drawpoints.py
+++ drawpoints.py
@@ -1,5 +1,5 @@
-def create_html(image,out='index.html'):
+def create_html(image, out='index.html'):
     with open('tail.txt') as g:
-        with open(out,'w') as f:
-            f.write('<br/><br/><img src="'+image+'" alt="Mouse coordinates"  id="imgid" style="float:left;" />')
+        with open(out, 'w') as f:
+            f.write('<html><body><div id="wrapper"><img src="'+image+'" alt="Mouse coordinates"  id="imgid"  />')
             f.write(g.read())
